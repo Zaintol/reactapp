3d48c464c626054f1ac513423f98a12819146aa8
Zaintol <zainibrahem5@gmail.com>
2024-09-27 17:39:57 +0400

    Added use context

--- Zain React App/src/App.tsx
+++ Zain React App/src/App.tsx
@@ -1,17 +1,21 @@
-import { useState } from 'react'
+import { useState, createContext } from 'react'
 import { BrowserRouter, Routes } from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Start from './components/Start'
+export const userContext = createContext<{value: string,setValue: (value: string)=>void}>({value: '',setValue: ()=>{}})
+ 
 function App() {
+  const [value, setValue] = useState("guest")
   const [count, setCount] = useState(0)
-
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Start />
-      </div>
-    </BrowserRouter>
+    <userContext.Provider value={{value,setValue}}>
+      <BrowserRouter>
+        <div className="App">
+          <Start />
+        </div>
+      </BrowserRouter>
+    </userContext.Provider>
   )
 }
 
--- Zain React App/src/components/counter.tsx
+++ Zain React App/src/components/counter.tsx
@@ -1,8 +1,13 @@
-import { useReducer, useState } from "react"
-
+import { useReducer, useState, useContext, useEffect } from "react"
+import { userContext } from "../App"
 export default function Counter(){
     const [count, setCount] = useState(0)
     const [state, dispatch] = useReducer(reducer, 0)
+    const value = useContext(userContext)
+    const context = useContext(userContext)
+    useEffect(()=>{
+        context.setValue("new")
+    },[])
     function reducer(state: number, action: {type: string, payload: number}){
         switch(action.type){
             case 'increment':
@@ -14,7 +19,7 @@ export default function Counter(){
         }
     }
     return <div>
-        <h1>Count: {state}</h1>
+        <h1>Count: {value.value}</h1>
 
         <button onClick={()=> dispatch({type: 'increment',payload: 1})}>Increment Reducer</button>
         <button onClick={() =>dispatch({type: 'decrement',payload: 1})}>Decrement Reducer</button>
